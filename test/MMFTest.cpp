6a807127eace75a92b5e7d6c375ad60214b1f24d
Igor Minin <igorm6387@gmail.com>
2016-08-07 14:47:56 +0300

    More unittests on MemroyMappedFile

--- test/MMFTest.cpp
+++ test/MMFTest.cpp
@@ -36,7 +36,7 @@ static bool ReadSmallFile(const string& fileName, string& fileContent)
 
 static bool DeleteFile(const string& fileName)
 {
-	return remove(fileName.c_str());
+	return remove(fileName.c_str()) == 0;
 }
 
 TEST(MMF, MissingFile)
@@ -46,28 +46,112 @@ TEST(MMF, MissingFile)
 	EXPECT_FALSE(mmf.Open("No such file"));
 }
 
-TEST(MMF, ReadSmallFile)
+TEST(MMF, ReadSmall)
 {
-	const string fileName = "SMALL.txt";
+	const string fileName = "SMALL00.txt";
 	const string inFileBody = "FILE BODY";
 	string outFileBodyTest;
 	ASSERT_TRUE(CreateSmallFile(fileName, inFileBody));
 	ASSERT_TRUE(ReadSmallFile(fileName, outFileBodyTest));
 	EXPECT_EQ(inFileBody, outFileBodyTest);
 
-	MemoryMappedFile mmf;
+	{
+		MemoryMappedFile mmf;
 
-	EXPECT_TRUE(mmf.Open(fileName));
-	EXPECT_TRUE(mmf.IsOpened());
+		EXPECT_TRUE(mmf.Open(fileName));
+		EXPECT_TRUE(mmf.IsOpened());
 
-	EXPECT_TRUE(mmf.GetData());
-	EXPECT_EQ(inFileBody.size(), mmf.GetFileSize());
-	EXPECT_EQ(inFileBody.size(), mmf.GetDataSize());
+		EXPECT_TRUE(mmf.GetData());
+		EXPECT_EQ(inFileBody.size(), mmf.GetFileSize());
+		EXPECT_EQ(inFileBody.size(), mmf.GetDataSize());
 	
-	const char* b = mmf.GetData();
-	const char* e = b + mmf.GetDataSize();
-	string outFileBody(b, e);
-	EXPECT_EQ(inFileBody, outFileBody);
+		const char* b = mmf.GetData();
+		const char* e = b + mmf.GetDataSize();
+		string outFileBody(b, e);
+		EXPECT_EQ(inFileBody, outFileBody);
+	}
+
+	DeleteFile(fileName);
+}
+
+TEST(MMF, ResizeSmall)
+{
+	const string fileName = "SMALL01.txt";
+	ASSERT_TRUE(CreateSmallFile(fileName, " "));
+
+	const string newFileBody = "Little brown fox";
+	{
+		MemoryMappedFile mmf;
+
+		EXPECT_TRUE(mmf.Open(fileName));
+		EXPECT_TRUE(mmf.IsOpened());
+		EXPECT_TRUE(mmf.GetData());
+		EXPECT_EQ(1u, mmf.GetFileSize());
+		EXPECT_EQ(1u, mmf.GetDataSize());
+
+		
+		stream_offset newSize = newFileBody.size();
+
+		EXPECT_TRUE(mmf.Resize(newSize));
+		EXPECT_EQ(newSize, mmf.GetFileSize());
+		EXPECT_EQ(newSize, mmf.GetDataSize());
+
+		copy(newFileBody.begin(), newFileBody.end(), mmf.GetData());
+	}
+
+	string testFileBody;
+	EXPECT_TRUE(ReadSmallFile(fileName, testFileBody));
+
+	EXPECT_EQ(newFileBody, testFileBody);
+
+	DeleteFile(fileName);
+}
+
+TEST(MMF, RemapLarge)
+{
+	const string fileName = "LARGE00.txt";
+	ASSERT_TRUE(CreateSmallFile(fileName, " "));
+
+	const size_t viewSize = 1024;
+	const stream_offset fileSize = 10ll * 1024*1024*1024;
+	const stream_offset messageOffset = 5ll * 1024*1024*1024;
+	const string message = "Five gigabytes";
+
+	// Write
+	{
+		MemoryMappedFile mmf(viewSize);
+		
+		EXPECT_TRUE(mmf.Open(fileName));
+
+		EXPECT_TRUE(mmf.Resize(fileSize));
+		EXPECT_TRUE(mmf.GetData());
+		EXPECT_EQ(fileSize, mmf.GetFileSize());
+		EXPECT_EQ(viewSize, mmf.GetDataSize());
+		
+		EXPECT_TRUE(mmf.Remap(messageOffset));
+		EXPECT_TRUE(mmf.GetData());
+		EXPECT_EQ(fileSize, mmf.GetFileSize());
+		EXPECT_EQ(viewSize, mmf.GetDataSize());
+
+		copy(message.begin(), message.end(), mmf.GetData());
+	}
+
+	// Read
+	{
+		MemoryMappedFile mmf(viewSize);
+
+		EXPECT_TRUE(mmf.Open(fileName, messageOffset));
+		EXPECT_TRUE(mmf.GetData());
+		EXPECT_EQ(fileSize, mmf.GetFileSize());
+		EXPECT_EQ(viewSize, mmf.GetDataSize());
+		
+		const char* b = mmf.GetData();
+		const char* e = b + message.size();
+
+		const string testMessage(b, e);
+
+		EXPECT_EQ(message, testMessage);
+	}
 
 	DeleteFile(fileName);
 }
