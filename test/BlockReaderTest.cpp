86b0de9aebb273d191c2fdf1eeb2c5570e480a9b
Igor Minin <igorm6387@gmail.com>
2016-08-10 20:33:10 +0300

    Delete test data after test

--- test/BlockReaderTest.cpp
+++ test/BlockReaderTest.cpp
@@ -1,6 +1,7 @@
 #include <libfsbox/BlockReader.hpp>
 
 #include "gtest/gtest.h"
+#include "TestUtils.h"
 
 using namespace std;
 using namespace BlockTypes;
@@ -9,12 +10,16 @@ TEST(BlockReader, ContainerHeader)
 {
 	const string fileName = "BLOCKREADER00.fsbox";
 
-	Container container;
-	ASSERT_TRUE(container.Open(fileName));
+	{
+		Container container;
+		ASSERT_TRUE(container.Open(fileName));
 
-	BlockReader blockReader(container);
+		BlockReader blockReader(container);
 
-	auto containerHeader = blockReader.Get<ContainerHeader>(0);
+		auto containerHeader = blockReader.Get<ContainerHeader>(0);
 
-	EXPECT_TRUE(containerHeader);
+		EXPECT_TRUE(containerHeader);
+	}
+
+	TestUtils::DeleteFile(fileName);
 }
