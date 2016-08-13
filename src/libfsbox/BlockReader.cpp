f9d75110ff5f0eaa4b5a6d396f83418bcd9b7f76
Igor Minin <igorm6387@gmail.com>
2016-08-13 17:10:06 +0300

    Additional checks in BlockReader

--- src/libfsbox/BlockReader.cpp
+++ src/libfsbox/BlockReader.cpp
@@ -39,13 +39,23 @@ namespace BlockTypes
 	{
 		return BlockType::FileEntry;
 	}
-}//namespace BlockTypes
 
-template <>
-bool CheckBlockType<BlockTypes::TypedBlock>(const BlockTypes::TypedBlock* block)
-{
-	return block->blockType >= BlockTypes::BlockType::FreeBlock &&
-		block->blockType <= BlockTypes::BlockType::FileHeader;
-}
+	template <>
+	bool CheckBlockType<BlockTypes::TypedBlock>(const BlockTypes::TypedBlock* block)
+	{
+		return block->blockType >= BlockTypes::BlockType::FreeBlock &&
+			block->blockType <= BlockTypes::BlockType::FileHeader;
+	}
+
+	uint32_t GetMaxBlockSize()
+	{
+		//TODO: May be we can guess better maximum size for block?
+		// Note that we can't make this value dependend on some particular CPU, because .fsbox files shud be portable from
+		// one machine to another
+		// So why? 64*1024 is the allocation granularity on my system, 20 is just reasonable value
+		// Also I like this number because it is close to 3.5 diskette size
+		return 64*1024*20;
+	}
+}//namespace BlockTypes
 
 }//namespace FsBox
--- src/libfsbox/BlockReader.hpp
+++ src/libfsbox/BlockReader.hpp
@@ -3,6 +3,7 @@
 
 #include "Container.h"
 #include "BlockTypes.h"
+#include "BlockAllocator.h"
 
 namespace FsBox
 {
@@ -31,21 +32,23 @@ namespace BlockTypes
 
 	template <>
 	BlockType GetBlockType<FileEntry>();
+
+	template <typename T>
+	bool CheckBlockType(const T* block)
+	{
+		return block->blockType == GetBlockType<T>();
+	}
+
+	template <>
+	bool CheckBlockType<BlockTypes::TypedBlock>(const TypedBlock* block);
+
+	uint32_t GetMaxBlockSize();
 } // namespace BlockTypes
-
-template <typename T>
-bool CheckBlockType(const T* block)
-{
-	return block->blockType == BlockTypes::GetBlockType<T>();
-}
-
-template <>
-bool CheckBlockType<BlockTypes::TypedBlock>(const BlockTypes::TypedBlock* block);
 
 class BlockReader : public boost::noncopyable
 {
 public:
-	BlockReader(Container& container) : _container(container) {}
+	BlockReader(Container& container) : _container(container), _mmf(container.GetFileMapping()) {}
 	
 	template <typename T>
 	T* Get(BlockHandle block)
@@ -55,7 +58,23 @@ public:
 		{
 			return nullptr;
 		}
-		return CheckBlockType(ptr) ? ptr : nullptr;
+		if (!BlockTypes::CheckBlockType(ptr))
+		{
+			return nullptr;
+		}
+		if (CheckBlockPayloadBorders(ptr))
+		{
+			return ptr;
+		}
+		// Remap view to fit block payload
+		_mmf.Remap(block);
+		// As remap invalidates all pointers, we should do the whole process again
+		ptr = UncheckedGet<T>(block);
+		if (!ptr)
+		{
+			return nullptr;
+		}
+		return CheckBlockPayloadBorders(ptr) ? ptr : nullptr;
 	}
 	
 	template <typename T>
@@ -74,28 +93,72 @@ private:
 	T* UncheckedGet(BlockHandle block)
 	{
 		std::lock_guard<std::recursive_mutex> lock(_container.GetLock());
-		MemoryMappedFile& mmf = _container.GetFileMapping();
-		if (!mmf.IsOpened())
+		if (!_mmf.IsOpened())
 		{
 			return nullptr;
 		}	
-		if (block > mmf.GetFileSize() - sizeof(T))
+		if (block > _mmf.GetFileSize() - sizeof(T))
 		{
 			return nullptr;
 		}
-		if (block < mmf.GetDataOffset() ||
-			block > mmf.GetDataOffset() + mmf.GetDataSize())
+		if (BlockNeedsRemap<T>(block))
 		{
-			if (!mmf.Remap(block))
+			if (!_mmf.Remap(block))
+			{
+				return nullptr;
+			}
+			if (BlockNeedsRemap<T>(block))
 			{
 				return nullptr;
 			}
 		}
-		size_t fixup = static_cast<size_t>(block - mmf.GetDataOffset());
-		return reinterpret_cast<T*>(mmf.GetData() + fixup);
+		size_t fixup = static_cast<size_t>(block - _mmf.GetDataOffset());
+		return reinterpret_cast<T*>(_mmf.GetData() + fixup);
+	}
+
+	template <typename T>
+	bool CheckBlockPayloadBorders(const T* ptr)
+	{
+		return CheckDataInMemoryView(ptr, sizeof(T));
+	}
+
+	template <>
+	bool CheckBlockPayloadBorders<BlockTypes::FreeBlock>(const BlockTypes::FreeBlock* ptr)
+	{
+		uint32_t fullSize = ptr->size;
+		if (fullSize > BlockTypes::GetMaxBlockSize())
+		{
+			return false;
+		}
+		return CheckDataInMemoryView(ptr, fullSize);
+	}
+
+	template <>
+	bool CheckBlockPayloadBorders<BlockTypes::FileEntry>(const BlockTypes::FileEntry* ptr)
+	{
+		uint32_t fullSize = ptr->entrySize;
+		if (fullSize > BlockTypes::GetMaxBlockSize())
+		{
+			return false;
+		}
+		return CheckDataInMemoryView(ptr, fullSize);
+	}
+
+	template <typename T>
+	bool BlockNeedsRemap(BlockHandle block)
+	{
+		return block < _mmf.GetDataOffset() ||
+			block > _mmf.GetDataOffset() + _mmf.GetDataSize() - sizeof(T);
+	}
+
+	bool CheckDataInMemoryView(const void* dataPtr, size_t dataSize)
+	{
+		return dataPtr >= _mmf.GetData() &&
+			dataPtr <= _mmf.GetData() + _mmf.GetDataSize() - dataSize;
 	}
 
 	Container& _container;
+	MemoryMappedFile& _mmf;
 };
 
 }//namespace FsBox
