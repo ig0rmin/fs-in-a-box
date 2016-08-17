8685b9ad9635c38492273767abe59e669d54988f
Igor Minin <igorm6387@gmail.com>
2016-08-17 18:18:45 +0300

    Fixed spelling error

--- src/libfsbox/BlockReader.cpp
+++ src/libfsbox/BlockReader.cpp
@@ -50,7 +50,7 @@ namespace BlockTypes
 	uint32_t GetMaxBlockSize()
 	{
 		//TODO: May be we can guess better maximum size for block?
-		// Note that we can't make this value dependend on some particular CPU, because .fsbox files shud be portable from
+		// Note that we can't make this value dependend on some particular CPU, because .fsbox files should be portable from
 		// one machine to another
 		// So why? 64*1024 is the allocation granularity on my system, 20 is just reasonable value
 		// Also I like this number because it is close to 3.5 diskette size
